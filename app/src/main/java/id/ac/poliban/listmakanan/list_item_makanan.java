db5a3ce31b4c81d00675f65264b7167ae380b9ed
unknown <radiansyah10>
2020-01-14 20:52:43 -0800

    Initial commit

---
+++ app/src/main/java/id/ac/poliban/listmakanan/list_item_makanan.java
@@ -0,0 +1,14 @@
+package id.ac.poliban.listmakanan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class list_item_makanan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_item_makanan);
+    }
+}
